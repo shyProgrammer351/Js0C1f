228acb1fa4add0e6d242839e1a43190cb6b1c383
shyProgrammer351 <123141646+shyProgrammer351@users.noreply.github.com>
2025-03-04 17:48:06 +0900

    Add files via upload

--- C0101.js
+++ C0101.js
@@ -1,2 +1,1 @@
-var sP3a1 = "<font color='red'><b>include </b></font> &nbsp;헤더파일(.h) 내용을 소스코드(.c)에 포함하는 명령문<br><br><font color='red'><b>stdio.h </b></font> &nbsp;standard input output의 줄임말로 printf나 scanf 등 표준입출력 관련 함수 정보를 가지고 있는 헤더파일<br><br><font color='red'><b>main </b></font> &nbsp;프로그램을 시작할 때 운영체제가 호출하는 함수<br><br><font color='red'><b>printf </b></font> &nbsp;print formatted의 줄임말로 전달받은 내용을 형식에 맞게 모니터에 출력하는 함수<br><br><font color='red'><b>\\n </b></font> &nbsp;줄바꿈(new line) 문자<br><br><font color='red'><b>return </b></font> &nbsp;함수 종료시 실행흐름이 함수를 호출했던 곳으로 되돌아감 <br>예를 들어, main 함수가 끝나면 main 함수를 호출한 운영체제로 제어흐름이 돌아가고 프로그램이 종료<br><br>";
-var sP3a2 =[[ "1",	"",	""],["2",	"Hello, World! \n",	""],["3",	"",	"" ],];
+const sP3a2 =[[ "1",	"",	""],["2",	"Hello, World! \n",	""],["3",	"",	"" ],];
